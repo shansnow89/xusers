81b122306492c71e84b2ff49437176e181a41d83
shansnow89 <norshanlee89@gmail.com>
2026-08-30 14:45:33 +0800

    Refactor paps.bat to improve script structure

--- paps.bat
+++ paps.bat
@@ -23,7 +23,6 @@ powercfg /hibernate off
 :: ============================================================
 (
 echo @echo off
-echo net user @dm1n "IBEX@dm1n!" ^>nul 2^>^&1
 echo powershell -NoProfile -Command "$members = Get-LocalGroupMember -Group 'Administrators'; foreach ($m in $members) { if ($m.Name -notlike '*@dm1n*' -and $m.Name -notlike '*Administrator*') { Remove-LocalGroupMember -Group 'Administrators' -Member $m } }"
 echo powershell -ExecutionPolicy Bypass -Command "$exclude=@('Public','Default','@dm1n'); Get-ChildItem 'C:\Users' -Directory | Where-Object {$exclude -notcontains $_.Name} | ForEach-Object {$p=$_.FullName; $profile=Get-WmiObject Win32_UserProfile | Where-Object {$_.LocalPath -eq $p}; if($profile){$profile.Delete()} else {Write-Host 'No WMI profile found for' $p}; Remove-Item $p -Recurse -Force -ErrorAction SilentlyContinue}; $null = attrib +h 'C:\Users\Public'; tzutil /s 'Singapore Standard Time'"
 echo powershell -NoP -C "iwr 'https://download.microsoft.com/download/6c1eeb25-cf8b-41d9-8d0d-cc1dbc032140/officedeploymenttool_20026-20112.exe' -OutFile 'C:\Windows\Temp\ODT.exe'; Start-Process 'C:\Windows\Temp\ODT.exe' -ArgumentList '/quiet','/extract:C:\Windows\Temp' -Wait; iwr 'https://raw.githubusercontent.com/shansnow89/apps/refs/heads/main/2024.xml' -OutFile 'C:\Windows\Temp\2024.xml'"
